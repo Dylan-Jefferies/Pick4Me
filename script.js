58bde826ac5bcba4dbca677c3eba1c796f6202dd
Dylan Jefferies <djefferies.sa@gmail.com>
2025-04-24 08:34:16 +0200

    spinner added; Image asset added

---
+++ script.js
@@ -0,0 +1,13 @@
+function rotateFunction() {
+	const min = 1024;
+	const max = 9999;
+	const deg = Math.floor(Math.random() * (max - min)) + min;
+	document.getElementById('box').style.transform = "rotate("+deg+"deg)";
+
+	const element = document.getElementById('mainbox');
+	element.classList.remove('animate');
+
+	setTimeout(function(){
+		element.classList.add('animate');
+	}, 5000);
+}
